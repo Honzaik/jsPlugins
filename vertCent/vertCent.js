344617ed6971613237c2db0b0bd2473cd8f4f22c
honzaik <honzaik@seznam.cz>
2014-04-13 14:47:29 +0200

    VertCent fix

--- vertCent/vertCent.js
+++ vertCent/vertCent.js
@@ -3,10 +3,20 @@ var VertCent = {};
 VertCent.center = function(){
 	var elementsToVertCent = $(".vertCent");
 	for(var i = 0; i < elementsToVertCent.length; i++){
-		var elementH = $(elementsToVertCent[i]).height();
-		var parentH = $(elementsToVertCent[i]).parent().height();
+		var element = $(elementsToVertCent[i]);
+		var parent = element.parent()
+		var elementH = element.height();
+		var parentH = parent.outerHeight();
+		var paddingTop = parseInt(parent.css("paddingTop"));
+		var offset = 0;
+		if(parent.children().length > 1){ // more than one element inside
+			var children = parent.children();
+			for(var j = 0; j < children.length; j++){
+				if(children[j] != element[0]) offset += $(children[j]).outerHeight();
+			}
+		}
 		if(elementH < parentH){
-			var gap = (parentH - elementH)/2;
+			var gap = ((parentH - offset - paddingTop) - elementH)/2;
 			$(elementsToVertCent[i]).css("marginTop", gap + "px");
 		}
 	}
